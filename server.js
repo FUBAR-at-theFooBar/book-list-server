78518951aa36c2c4a9829413453b246b00422264
strawbee <yuejiao.hou@gmail.com>
2017-11-07 09:44:38 -0800

    test

--- server.js
+++ server.js
@@ -2,20 +2,21 @@
 
 const express = require('express');
 const pg = require('pg');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 3000;
-const conString = 'postgres://localhost:5432';
-const client = new pg.Client(conString);
+const PORT = process.env.PORT;
+
+const CLIENT_URL = process.env.CLIENT_URL;
+
+const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
-client.on('error', err => {
-  console.error(err);
-});
+client.on('error', err => console.error(err));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 
 app.get('/test', (request, response) => response.send('Hello world.'));
-app.get('*', (request,response) => response.send('404'));
-
+app.get('*', (req, res) => res.redirect(CLIENT_URL));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
