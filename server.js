b0232effd9b2aa193404159ea9c3239158c5ba1d
strawbee <yuejiao.hou@gmail.com>
2017-11-07 11:49:17 -0800

    testing heroku

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const pg = require('pg');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT;
 
@@ -12,11 +11,18 @@ const CLIENT_URL = process.env.CLIENT_URL;
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
 app.get('/test', (request, response) => response.send('Hello world.'));
 app.get('/*', (req, res) => res.send('404'));
+
+
+app.get('/api/v1/books', (request, response) => {
+  client.query(`
+    SELECT book_id, title, author, image_url FROM books;`
+  )
+  .then(result => response.send(result.rows))
+  .catch(console.error);
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
