1c33ea486209812a0a85a6487edddf4eeee02486
strawbee <yuejiao.hou@gmail.com>
2017-11-07 16:36:09 -0800

    fleshing out the server.js code

--- server.js
+++ server.js
@@ -1,39 +1,34 @@
 'use strict';
 
-//app dependencies
+// app dependencies
 const express = require('express');
 const pg = require('pg');
 const cors = require('cors');
 
-//app setup
+// app setup
 const app = express();
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
 
-//database setup
+// database setup
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 client.on('error', err => console.error(err));
 
-//middleware
+// middleware
 app.use(cors());
 
-//api endpoints
-// app.get('*', (req,res)=> res.redirect(CLIENT_URL)); //instr code
+// api endpoints
 app.get('/', (req, res) => res.redirect(CLIENT_URL));
-app.get('/test', (request, response) => response.send('Hello world.'));
 app.get('/*', (req, res) => res.send('404'));
 
-
+// When the client (view) makes an Ajax call to /api/v1/books, returns all results except for the description.
 app.get('/api/v1/books', (request, response) => {
   client.query(`
-    SELECT book_id, title, author, image_url FROM books;`
+    SELECT book_id, title, author, image_url, isbn FROM books;`
   )
-  // .then(console.log(response))
-  // .catch(console.error);
-    .then(results => response.send(results.rows))
-    .catch(console.error);
+  .then(results -> res.send(results.rows))
+  .catch(console.error);
 });
-
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
