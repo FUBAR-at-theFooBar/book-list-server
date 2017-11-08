c37119f507f887c1fb5e3fda319744367d98bd98
strawbee <yuejiao.hou@gmail.com>
2017-11-07 20:05:11 -0800

    uncommenting

--- server.js
+++ server.js
@@ -4,8 +4,8 @@
 const express = require('express');
 const pg = require('pg');
 const cors = require('cors');
-// const fs = require('fs');
-// const bodyParser = require('body-parser');
+const fs = require('fs');
+const bodyParser = require('body-parser');
 
 // app setup
 const app = express();
@@ -19,8 +19,8 @@ client.on('error', err => console.error(err));
 
 // middleware
 app.use(cors());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // api endpoints
 // When the client (view) makes an Ajax call to /api/v1/books, returns all results except for the description.
@@ -34,44 +34,44 @@ app.get('/api/v1/books', (request, response) => {
 
 app.get('/*', (req, res) => res.redirect(CLIENT_URL));
 
-// loadDB();
+loadDB();
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
 
-// function loadBooks() {
-//   console.log('loadBooks');
-//   client.query('SELECT COUNT(*) FROM books')
-//     .then(result => {
-//       if(!parseInt(result.rows[0].count)) {
-//         fs.readFile(`${CLIENT_URL}/data/books.json`, (err, fd) => {
-//           JSON.parse(fd.toString()).forEach(ele => {
-//             client.query(`
-//             INSERT INTO
-//             books(title, author, image_url, isbn, description)
-//             SELECT $1, $2, $3, $4, $5
-//             `,
-//               [ele.title, ele.author, ele.image_url, ele.isbn, ele.description]
-//             )
-//               .catch(console.error);
-//           })
-//         })
-//       }
-//     })
-// }
-//
-// function loadDB() {
-//   console.log('loadDB');
-//   client.query(`
-//     CREATE TABLE IF NOT EXISTS
-//     books (
-//       book_id SERIAL PRIMARY KEY,
-//       title VARCHAR(255) NOT NULL,
-//       author VARCHAR(255) NOT NULL,
-//       image_url TEXT,
-//       isbn TEXT,
-//       description TEXT
-//     );`
-//   )
-//     .then(loadBooks)
-//     .catch(console.error)
-// }
+function loadBooks() {
+  console.log('loadBooks');
+  client.query('SELECT COUNT(*) FROM books')
+    .then(result => {
+      if(!parseInt(result.rows[0].count)) {
+        fs.readFile(`${CLIENT_URL}/data/books.json`, (err, fd) => {
+          JSON.parse(fd.toString()).forEach(ele => {
+            client.query(`
+            INSERT INTO
+            books(title, author, image_url, isbn, description)
+            SELECT $1, $2, $3, $4, $5
+            `,
+              [ele.title, ele.author, ele.image_url, ele.isbn, ele.description]
+            )
+              .catch(console.error);
+          })
+        })
+      }
+    })
+}
+
+function loadDB() {
+  console.log('loadDB');
+  client.query(`
+    CREATE TABLE IF NOT EXISTS
+    books (
+      book_id SERIAL PRIMARY KEY,
+      title VARCHAR(255) NOT NULL,
+      author VARCHAR(255) NOT NULL,
+      image_url TEXT,
+      isbn TEXT,
+      description TEXT
+    );`
+  )
+    .then(loadBooks)
+    .catch(console.error)
+}
