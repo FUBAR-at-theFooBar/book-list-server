e4f22c862f26d02c8c3d3bb722045414fd967834
Nicholas Carignan <nick.carignan@sbcglobal.net>
2017-11-08 10:24:15 -0800

    reconfiguring post routes

--- server.js
+++ server.js
@@ -44,16 +44,17 @@ app.get('/api/v1/books', (request, response) => {
 // });
 //
 //
-// app.post('api/v1/books'), (request, response) => {
-//   client.query(`
-//     INSERT INTO books(title, author, image_url, isbn, description)
-//     VALUES($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING`,
-//     [request.body.title, request.body.author, request.body.image_url, request.body.isbn, request.body.description],
-//     function(err) {
-//       if(err) console.error(err);
-//     }
-//   )
-// };
+app.post('api/v1/books'), (request, response) => {
+  client.query(`
+    INSERT INTO books(title, author, image_url, isbn, description)
+    VALUES($1, $2, $3, $4, $5) ON CONFLICT DO NOTHING`,
+    [request.body.title, request.body.author, request.body.image_url, request.body.isbn, request.body.description],
+    function(err) {
+      if(err) console.error(err);
+    }
+  )
+    .then(response.send('finished posting new book'))
+};
 
 app.get('/*', (req, res) => res.redirect(CLIENT_URL));
 
