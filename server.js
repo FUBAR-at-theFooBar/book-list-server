c4f2f50cf61e163b1af4fb2e087eba904a948ee3
strawbee <yuejiao.hou@gmail.com>
2017-11-09 16:02:28 -0800

    local db works

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.use(cors());
 // api endpoints
 // When the client (view) makes an Ajax call to /api/v1/books, returns all results except for the description.
 app.get('/api/v1/books', (request, response) => {
+  console.log('server get');
   client.query(`
     SELECT * FROM books;`
   )
@@ -60,45 +61,45 @@ app.post('/api/v1/books', bodyParser, (request, response) => {
 
 app.get('/*', (req, res) => res.redirect(CLIENT_URL));
 
-loadDB();
+// loadDB();
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
 
-function loadBooks() {
-  console.log('loadBooks');
-  client.query('SELECT COUNT(*) FROM books')
-    .then(result => {
-      if(!parseInt(result.rows[0].count)) {
-        fs.readFile(`${CLIENT_URL}/data/books.json`, (err, fd) => {
-          JSON.parse(fd.toString()).forEach(ele => {
-            client.query(`
-            INSERT INTO
-            books(title, author, image_url, isbn, description)
-            SELECT $1, $2, $3, $4, $5
-            `,
-              [ele.title, ele.author, ele.image_url, ele.isbn, ele.description]
-            )
-              .catch(console.error);
-          })
-        })
-      }
-    })
-}
-
-/* Why doesn't this work!? */
-function loadDB() {
-  console.log('loadDB');
-  client.query(`
-    CREATE TABLE IF NOT EXISTS
-    books (
-      book_id SERIAL PRIMARY KEY,
-      title VARCHAR(255) NOT NULL,
-      author VARCHAR(255) NOT NULL,
-      image_url TEXT,
-      isbn TEXT,
-      description TEXT
-    );`
-  )
-    .then(loadBooks)
-    .catch(console.error)
-}
+// function loadBooks() {
+//   console.log('loadBooks');
+//   client.query('SELECT COUNT(*) FROM books')
+//     .then(result => {
+//       if(!parseInt(result.rows[0].count)) {
+//         fs.readFile(`${CLIENT_URL}/data/books.json`, (err, fd) => {
+//           JSON.parse(fd.toString()).forEach(ele => {
+//             client.query(`
+//             INSERT INTO
+//             books(title, author, image_url, isbn, description)
+//             SELECT $1, $2, $3, $4, $5
+//             `,
+//               [ele.title, ele.author, ele.image_url, ele.isbn, ele.description]
+//             )
+//               .catch(console.error);
+//           })
+//         })
+//       }
+//     })
+// }
+//
+// /* Why doesn't this work!? */
+// function loadDB() {
+//   console.log('loadDB');
+//   client.query(`
+//     CREATE TABLE IF NOT EXISTS
+//     books (
+//       book_id SERIAL PRIMARY KEY,
+//       title VARCHAR(255) NOT NULL,
+//       author VARCHAR(255) NOT NULL,
+//       image_url TEXT,
+//       isbn TEXT,
+//       description TEXT
+//     );`
+//   )
+//     .then(loadBooks)
+//     .catch(console.error)
+// }
